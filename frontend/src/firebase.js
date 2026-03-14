4f35ea391d46c497fe99d2b48ff6c242656e0d61
mkadomoto <mkadomoto@frba.utn.edu.ar>
2026-03-14 18:25:47 -0300

    Update firebase.js

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -1,16 +1,16 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-
-import {
- getFirestore,
- collection,
- addDoc,
- getDocs,
- orderBy,
- query,
- serverTimestamp,
- doc,
- deleteDoc
-} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
- apiKey: "AIzaSyBLCGxMnraxFH
+  apiKey: "AIzaSyBLCGxMnraxFHb33OYl3EQXkWiCes_9FFU",
+  authDomain: "byk3dimpresiones-746d4.firebaseapp.com",
+  projectId: "byk3dimpresiones-746d4",
+  storageBucket: "byk3dimpresiones-746d4.firebasestorage.app",
+  messagingSenderId: "948064780109",
+  appId: "1:948064780109:web:b66dcba2371c9b17180104",
+  measurementId: "G-M4H3W4XHST"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app);
